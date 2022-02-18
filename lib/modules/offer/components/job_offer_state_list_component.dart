4ce4b16a37445f0fdd13e3f128083ecdae35b678
Sotheng Chheang <sotheng@viazul.hk>
2022-02-18 15:50:16 +0700

    update logic in top offertype for rejected item equal 0 will hide it in the UI.

--- lib/modules/offer/components/job_offer_state_list_component.dart
+++ lib/modules/offer/components/job_offer_state_list_component.dart
@@ -27,81 +27,80 @@ class JobOfferStateListComponent extends StatelessWidget {
         scrollDirection: Axis.horizontal,
         itemBuilder: (BuildContext context, int index) {
           final FieldModel item = jobOfferStateList![index];
-          return
-              // item.total == 0
-              //     ? Container()
-              //     :
-              Obx(
-            () => Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: AppSize.s4,
-                vertical: AppSize.s14,
-              ),
-              child: GestureDetector(
-                onTap: () {
-                  offerController!.selectjobOfferState(
-                    state: item,
-                  );
-                },
-                child: CustomBoxWidget(
-                  backgroundColor: containerColorState(
-                    object1: '${item.label}',
-                    object2: offerController!.jobOfferStateSelected.value.label,
-                    isTextColor: false,
-                  ),
-                  borderWidth: 0.5,
-                  borderRadius: AppSize.s20,
-                  leftPadding: AppSize.s16,
-                  rightPadding: AppSize.s16,
-                  insideObj: Row(
-                    children: [
-                      CustomTextWidget(
-                        text: '${item.label}',
-                        // fontSize: AppSize.s14,
-                        fontWeight: FontWeightManager.medium,
-                        color: containerColorState(
-                          object1: '${item.label}',
-                          object2: offerController!
-                              .jobOfferStateSelected.value.label,
-                          unEqualStateColor: ColorsManager.primaryBlue,
-                        ),
-                        marginRight: AppSize.s4,
-                      ),
-                      CustomBoxWidget(
-                        borderRadius: AppSize.s16,
-                        topPadding: AppSize.s2,
-                        bottomPadding: AppSize.s2,
-                        leftPadding: AppSize.s0,
-                        rightPadding: AppSize.s0,
+          return item.label == 'Rejected' && item.total == 0
+              ? Container()
+              : Obx(
+                  () => Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: AppSize.s4,
+                      vertical: AppSize.s14,
+                    ),
+                    child: GestureDetector(
+                      onTap: () {
+                        offerController!.selectjobOfferState(
+                          state: item,
+                        );
+                      },
+                      child: CustomBoxWidget(
                         backgroundColor: containerColorState(
                           object1: '${item.label}',
                           object2: offerController!
                               .jobOfferStateSelected.value.label,
-                          equalStateColor: ColorsManager.primary,
-                          unEqualStateColor: ColorsManager.white,
                           isTextColor: false,
                         ),
-                        insideObj: CustomTextWidget(
-                          text: '${item.total}',
-                          fontSize: AppSize.s12,
-                          color: containerColorState(
-                            object1: '${item.label}',
-                            object2: offerController!
-                                .jobOfferStateSelected.value.label,
-                            equalStateColor: ColorsManager.primaryBlue,
-                            unEqualStateColor: ColorsManager.white,
-                          ),
-                          textAlign: TextAlign.center,
-                          marginLeft: AppSize.s6,
-                          marginRight: AppSize.s6,
+                        borderWidth: 0.5,
+                        borderRadius: AppSize.s20,
+                        leftPadding: AppSize.s16,
+                        rightPadding: AppSize.s16,
+                        insideObj: Row(
+                          children: [
+                            CustomTextWidget(
+                              text: '${item.label}',
+                              // fontSize: AppSize.s14,
+                              fontWeight: FontWeightManager.medium,
+                              color: containerColorState(
+                                object1: '${item.label}',
+                                object2: offerController!
+                                    .jobOfferStateSelected.value.label,
+                                unEqualStateColor: ColorsManager.primaryBlue,
+                              ),
+                              marginRight: AppSize.s4,
+                            ),
+                            CustomBoxWidget(
+                              borderRadius: AppSize.s16,
+                              topPadding: AppSize.s2,
+                              bottomPadding: AppSize.s2,
+                              leftPadding: AppSize.s0,
+                              rightPadding: AppSize.s0,
+                              backgroundColor: containerColorState(
+                                object1: '${item.label}',
+                                object2: offerController!
+                                    .jobOfferStateSelected.value.label,
+                                equalStateColor: ColorsManager.primary,
+                                unEqualStateColor: ColorsManager.white,
+                                isTextColor: false,
+                              ),
+                              insideObj: CustomTextWidget(
+                                text: '${item.total}',
+                                fontSize: AppSize.s12,
+                                color: containerColorState(
+                                  object1: '${item.label}',
+                                  object2: offerController!
+                                      .jobOfferStateSelected.value.label,
+                                  equalStateColor: ColorsManager.primaryBlue,
+                                  unEqualStateColor: ColorsManager.white,
+                                ),
+                                textAlign: TextAlign.center,
+                                marginLeft: AppSize.s6,
+                                marginRight: AppSize.s6,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    ],
+                    ),
                   ),
-                ),
-              ),
-            ),
-          );
+                );
         },
       ),
     );

40913e84924007c57ff0dfb64f8f727f2af62254
Sotheng Chheang <sotheng@viazul.hk>
2022-03-09 11:57:10 +0700

    finished Searching Keywords

--- lib/modules/offer/offer_search/offer_search_page.dart
+++ lib/modules/offer/offer_search/offer_search_page.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:get/get.dart';
 
 import '../../../core/core.dart';
 import '../../modules.dart';
 
 class OfferSearchPage extends SearchDelegate {
-  // OfferSearchPage();
-
   final feedController = Get.find<OfferFeedController>();
 
   @override
@@ -15,7 +14,10 @@ class OfferSearchPage extends SearchDelegate {
       IconButton(
         icon: const Icon(Icons.clear),
         onPressed: () {
-          query = feedController.keywordForSearch.value;
+          assignKeywordtoVariable();
+          // debugPrint(
+          //   'buildActions keywordForSearch:: ${feedController.keywordForSearch.value}',
+          // );
         },
       ),
     ];
@@ -36,18 +38,38 @@ class OfferSearchPage extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(
-        top: AppSize.s10,
-      ),
-      child: OfferFeedListComponent(),
-    );
+    SchedulerBinding.instance!.addPostFrameCallback((_) {
+      assignKeywordtoVariable();
+      feedController.getFeedsDataState();
+      Get.back();
+    });
+
+    return Container();
+    // return Container(
+    //   padding: const EdgeInsets.only(
+    //     top: AppSize.s10,
+    //   ),
+    //   child: OfferFeedListComponent(),
+    // );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
+    // assignKeywordtoVariable(isAssignToQuery: true);
+    // debugPrint(
+    //   'buildSuggestions keywordForSearch:: ${feedController.keywordForSearch.value}',
+    // );
     return const Center(
       child: CustomTextWidget(text: 'search under construction!!'),
     );
+  }
+
+  void assignKeywordtoVariable({bool? isAssignToQuery = false}) {
+    isAssignToQuery == true
+        ? query = feedController.keywordForSearch.value.trim()
+        : feedController.keywordForSearch.value = query.trim();
+    // debugPrint(
+    //   'assignKeywordtoVariable keywordForSearch:: ${feedController.keywordForSearch.value}',
+    // );
   }
 }

51642f5a5da9ec23295e8548097338a6d7828d3a
Sotheng Chheang <sotheng@viazul.hk>
2022-06-20 15:51:55 +0700

    [POOUL-998][Changing Layout on Skill Section UI in Profile UI Page]

--- lib/modules/profile/components/profile_skill_component.dart
+++ lib/modules/profile/components/profile_skill_component.dart
@@ -28,46 +28,66 @@ class ProfileSkillComponent extends StatelessWidget {
           Get.toNamed(addOrEditSkillRoute!, arguments: [Keys.addOperation]),
         },
       ),
-      child: Row(
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Expanded(
-            flex: 50,
-            child: Container(
-              padding: const EdgeInsets.fromLTRB(24.0, 0.0, 8.0, 0.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  CustomTextWidget(
-                    text: 'profile.hardSkills'.tr,
-                    marginBottom: 5.0,
-                    fontWeight: FontWeight.w600,
-                    color: ColorsManager.grey850,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: skillList!
-                        .map(
-                          (e) => e.type == Type.hardSkill
-                              ? CustomTextWidget(
-                                  text: e.level == null || e.level == 0
-                                      ? '${e.displayLabelAndCategory}'
-                                      : '${e.displayLabelAndCategory} \n  - ${e.displayLevel}',
-                                  color: ColorsManager.grey800,
-                                  marginBottom: 8.0,
-                                  maxLine: 5,
-                                )
-                              : Container(),
-                        )
-                        .toList(),
-                  )
-                ],
-              ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(24.0, 0.0, 8.0, 0.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomTextWidget(
+                  text: 'profile.hardSkills'.tr,
+                  marginBottom: 5.0,
+                  fontWeight: FontWeight.w600,
+                  color: ColorsManager.grey850,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: skillList!
+                      .map(
+                        (e) => e.type == Type.hardSkill
+                            ? Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Expanded(
+                                    child: CustomTextWidget(
+                                      text: '${e.displayLabelAndCategory}',
+                                      color: ColorsManager.grey800,
+                                      marginBottom: AppSize.s10,
+                                      maxLine: 5,
+                                      marginRight: AppSize.s16,
+                                    ),
+                                  ),
+                                  CustomTextWidget(
+                                    text: '${e.displayLevel}',
+                                    fontWeight: FontWeightManager.medium,
+                                    color: ColorsManager.grey800,
+                                    marginBottom: AppSize.s10,
+                                    marginLeft: AppSize.s16,
+                                    maxLine: 5,
+                                  ),
+                                ],
+                              )
+                            // CustomTextWidget(
+                            //     text: e.level == null || e.level == 0
+                            //         ? '${e.displayLabelAndCategory}'
+                            //         : '${e.displayLabelAndCategory} \n  - ${e.displayLevel}',
+                            //     color: ColorsManager.grey800,
+                            //     marginBottom: 8.0,
+                            //     maxLine: 5,
+                            //   )
+                            : Container(),
+                      )
+                      .toList(),
+                )
+              ],
             ),
           ),
-          Expanded(
-            flex: 50,
-            child: Container(
+          if (skillList!.where((e) => e.type == Type.softSkill).isEmpty)
+            Container()
+          else
+            Container(
               padding: const EdgeInsets.fromLTRB(24.0, 0.0, 8.0, 0.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -96,9 +116,79 @@ class ProfileSkillComponent extends StatelessWidget {
                 ],
               ),
             ),
-          ),
         ],
       ),
+      // child: Row(
+      //   crossAxisAlignment: CrossAxisAlignment.start,
+      //   children: [
+      //     Expanded(
+      //       flex: 50,
+      //       child: Container(
+      //         padding: const EdgeInsets.fromLTRB(24.0, 0.0, 8.0, 0.0),
+      //         child: Column(
+      //           crossAxisAlignment: CrossAxisAlignment.start,
+      //           children: [
+      //             CustomTextWidget(
+      //               text: 'profile.hardSkills'.tr,
+      //               marginBottom: 5.0,
+      //               fontWeight: FontWeight.w600,
+      //               color: ColorsManager.grey850,
+      //             ),
+      //             Column(
+      //               crossAxisAlignment: CrossAxisAlignment.start,
+      //               children: skillList!
+      //                   .map(
+      //                     (e) => e.type == Type.hardSkill
+      //                         ? CustomTextWidget(
+      //                             text: e.level == null || e.level == 0
+      //                                 ? '${e.displayLabelAndCategory}'
+      //                                 : '${e.displayLabelAndCategory} \n  - ${e.displayLevel}',
+      //                             color: ColorsManager.grey800,
+      //                             marginBottom: 8.0,
+      //                             maxLine: 5,
+      //                           )
+      //                         : Container(),
+      //                   )
+      //                   .toList(),
+      //             )
+      //           ],
+      //         ),
+      //       ),
+      //     ),
+      //     Expanded(
+      //       flex: 50,
+      //       child: Container(
+      //         padding: const EdgeInsets.fromLTRB(24.0, 0.0, 8.0, 0.0),
+      //         child: Column(
+      //           crossAxisAlignment: CrossAxisAlignment.start,
+      //           children: [
+      //             CustomTextWidget(
+      //               text: 'profile.softSkills'.tr,
+      //               color: ColorsManager.grey850,
+      //               marginBottom: 5.0,
+      //               fontWeight: FontWeight.w600,
+      //             ),
+      //             Column(
+      //               crossAxisAlignment: CrossAxisAlignment.start,
+      //               children: skillList!
+      //                   .map(
+      //                     (e) => e.type == Type.softSkill
+      //                         ? CustomTextWidget(
+      //                             text: '• ${e.label}',
+      //                             color: ColorsManager.grey800,
+      //                             marginBottom: 8.0,
+      //                             maxLine: 3,
+      //                           )
+      //                         : Container(),
+      //                   )
+      //                   .toList(),
+      //             )
+      //           ],
+      //         ),
+      //       ),
+      //     ),
+      //   ],
+      // ),
     );
   }
 }

5e75984b24a371e591ea9e05333e912b51c53b06
Chheang Sotheng <chheangsotheng@yahoo.com>
2022-08-19 11:18:59 +0700

    [Fix] url launcher and [update] languages in forget password page

--- lib/core/utils/url_laucher_utils.dart
+++ lib/core/utils/url_laucher_utils.dart
@@ -13,37 +13,60 @@ Future urlLauncherUtils({
   String? emailBodyMessage = '',
   bool? inApp = false,
 }) async {
-  String thingWillLaunch = '';
+  late Uri thingWillLaunch;
 
   if (laucherType == LauncherType.web) {
-    try {
-      if (await canLaunch(thingToLaunch.toString())) {
-        await launch(
-          thingToLaunch.toString(),
-          forceSafariVC: inApp, //iOS
-          forceWebView: inApp!, //Android
-          enableJavaScript: true, //Android
-        );
-      } else {
-        customSnackbar(
-          msgTitle: 'Unable to launch',
-          msgContent: '',
-          bgColor: ColorsManager.red,
-        );
-      }
-    } catch (e) {
-      throw 'Could not lauch $e';
+    final web = Uri.parse(
+      thingToLaunch.toString(),
+      // 'https://google.com/',
+    );
+    if (await canLaunchUrl(web)) {
+      launchUrl(web);
+    } else {
+      customSnackbar(
+        msgTitle: 'Unable to launch',
+        msgContent: '',
+        bgColor: ColorsManager.red,
+      );
+      throw 'Could not launch $web';
     }
+    // try {
+    //   if (await canLaunch(thingToLaunch.toString())) {
+    //     await launch(
+    //       thingToLaunch.toString(),
+    //       forceSafariVC: inApp, //iOS
+    //       forceWebView: inApp!, //Android
+    //       enableJavaScript: true, //Android
+    //     );
+    //   } else {
+    //     customSnackbar(
+    //       msgTitle: 'Unable to launch',
+    //       msgContent: '',
+    //       bgColor: ColorsManager.red,
+    //     );
+    //   }
+    // } catch (e) {
+    //   throw 'Could not lauch $e';
+    // }
   } else {
     if (laucherType == LauncherType.phone) {
-      final Uri launchUri = Uri(
-        // scheme: Platform.isIOS ? "tel://" : "tel:",
-        scheme: 'tel',
-        path: thingToLaunch,
-      );
-      thingWillLaunch =
-          "${Platform.isIOS ? "tel://" : "tel:"}${launchUri.path}";
-      // thingWillLaunch = 'tel://$thingToLaunch';
+      final String prefix = Platform.isIOS ? "tel://" : "tel:";
+      final call =
+          // Uri.parse('tel:$thingToLaunch');
+          Uri.parse('$prefix$thingToLaunch');
+      // if (await canLaunchUrl(call)) {
+      //   launchUrl(call);
+      // } else {
+      //   throw 'Could not launch $call';
+      // }
+      // final Uri launchUri = Uri(
+      //   // scheme: Platform.isIOS ? "tel://" : "tel:",
+      //   scheme: 'tel',
+      //   path: thingToLaunch,
+      // );
+      // thingWillLaunch =
+      //     "${Platform.isIOS ? "tel://" : "tel:"}${launchUri.path}";
+      thingWillLaunch = call;
 
       // await _urlLauncher(url: launchUri.toString());
     } else if (laucherType == LauncherType.email) {
@@ -56,21 +79,41 @@ Future urlLauncherUtils({
             .join('&');
       }
 
-      final Uri emailLaunchUri = Uri(
+      final email = Uri(
         scheme: 'mailto',
+        // path: 'codoweb.tech@gmail.com',
+        // query: 'subject=App Feedback&body=App Version 3.23'//'subject=Hello&body=Test',
         path: thingToLaunch,
-        query:
-            encodeQueryParameters(<String, String>{'subject': '$emailSubject'}),
+        query: encodeQueryParameters(
+          <String, String>{'subject': '$emailSubject', 'body': ''},
+        ),
       );
-      thingWillLaunch = emailLaunchUri.toString();
+      // if (await canLaunchUrl(email)) {
+      //   launchUrl(email);
+      // } else {
+      //   throw 'Could not launch $email';
+      // }
+      //  final Uri emailLaunchUri = Uri(
+      //   scheme: 'mailto',
+      //   path: thingToLaunch,
+      //   query:
+      //       encodeQueryParameters(<String, String>{'subject': '$emailSubject'}),
+      // );
+      thingWillLaunch = email;
       // await _urlLauncher(url: emailLaunchUri.toString());
     }
     if (laucherType == LauncherType.sms) {
-      thingWillLaunch = 'sms:$thingToLaunch';
+      final sms = Uri.parse('sms:$thingToLaunch');
+      // if (await canLaunchUrl(sms)) {
+      //   launchUrl(sms);
+      // } else {
+      //   throw 'Could not launch $sms';
+      // }
+      thingWillLaunch = sms;
     }
     try {
-      if (await canLaunch(thingWillLaunch)) {
-        await launch(thingWillLaunch);
+      if (await canLaunchUrl(thingWillLaunch)) {
+        launchUrl(thingWillLaunch);
       } else {
         customSnackbar(
           msgTitle: 'Unable to launch',

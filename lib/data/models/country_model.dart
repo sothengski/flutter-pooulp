93beede1a533fa2f1466fc4d3ec914b56357b0e0
Chheang Sotheng <chheangsotheng@yahoo.com>
2022-12-19 13:39:28 +0700

    [update] country_model structure

--- lib/data/models/country_model.dart
+++ lib/data/models/country_model.dart
@@ -1,14 +1,16 @@
 class CountryModel {
   final String? name;
+  final String? frName;
   final String? isoCode;
   final String? iso3Code;
   final String? phoneCode;
 
   const CountryModel({
+    this.name,
+    this.frName,
     this.isoCode,
     this.iso3Code,
     this.phoneCode,
-    this.name,
   });
 
   /// returns "+ [phoneCode]"
@@ -16,6 +18,7 @@ class CountryModel {
 
   factory CountryModel.fromJson(Map<String, String> json) => CountryModel(
         name: json['name'],
+        frName: json['frName'],
         isoCode: json['isoCode'],
         iso3Code: json['iso3Code'],
         phoneCode: json['phoneCode'],
@@ -25,9 +28,10 @@ class CountryModel {
   String toString() {
     return '''
     CountryModel(
-      name: $name, 
-      isoCode: $isoCode, 
-      iso3Code: $iso3Code, 
+      name: $name,
+      frName: $frName,
+      isoCode: $isoCode,
+      iso3Code: $iso3Code,
       phoneCode: $phoneCode)''';
   }
 }
